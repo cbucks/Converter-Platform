a089ed9186929f103f6164ef607e0c1de3137120
cbucks <cbucks.blockchain@gmail.com>
2020-01-16 16:45:23 +0800

    feat: Added polling in getting on rates

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -26,6 +26,8 @@ class ConverterComponent extends React.Component {
     this.onChangeFrom = this.onChangeFrom.bind(this);
     this.onChangeTo = this.onChangeTo.bind(this);
     this.getRate = this.getRate.bind(this);
+    this.onChangeFromOption = this.onChangeFromOption.bind(this);
+    this.onChangeToOption = this.onChangeToOption.bind(this);
   }
 
   componentDidMount() {
@@ -36,8 +38,10 @@ class ConverterComponent extends React.Component {
   }
 
   getRate() {
+    console.log('to option:', this.state.toOption);
     axios.get(`${Development.baseUrl}/market/price/${this.state.toOption}`)
       .then((result) => {
+        console.log(result);
         this.setState({rate: result.data.result.price});
         this.onChangeFrom(this.state.from);
         this.onChangeTo(this.state.to);
@@ -45,30 +49,32 @@ class ConverterComponent extends React.Component {
       .catch((err) => console.log(err))
   }
 
+  onChangeFromOption(value) {
+    this.setState({fromOption: value});
+    this.getRate();
+  }
+
+  onChangeToOption(value) {
+    // return new Promise((resolve, reject) => {
+    //   this.setState({toOption: value});
+    // })
+    // .then(() => this.getRate());
+    this.setState({toOption: value});
+    setTimeout(() => {
+      this.getRate();
+    }, 500)
+  }
+
   onChangeFrom(value) {
     this.setState({from: value});
     const conversion = value * this.state.rate;
     this.setState({to: conversion});
-    // axios.get(`${Development.baseUrl}/market/price/${this.state.toOption}`)
-    //   .then((result) => {
-    //     const conversion = value * result.data.result.price;
-    //     this.setState({to: conversion});
-    //   })
-    //   .catch((err) => console.log(err))
   }
 
   onChangeTo(value) {
     this.setState({to: value});
     const conversion = value / this.state.rate;
     this.setState({from: conversion});
-    // axios.get(`${Development.baseUrl}/market/price/${this.state.toOption}`)
-    //   .then((result) => {
-    //     console.log('Result:', result.data.result.price);
-    //     const conversion = value / result.data.result.price;
-    //     console.log('VALUE:', conversion);
-    //     this.setState({from: conversion});
-    //   })
-    //   .catch((err) => console.log(err))
   }
 
   render() {
@@ -77,12 +83,17 @@ class ConverterComponent extends React.Component {
         <Input onChange={(e) => {
           this.onChangeFrom(Number(e.target.value));
         }} size="massive" value={this.state.from} />
-        <Select className="converter-select" size="massive" options={fromOptions} value={this.state.fromOption} />
+        <Select onChange={(e, e2) => {
+          this.onChangeFromOption(e2.value);
+        }} className="converter-select" size="massive" options={fromOptions} value={this.state.fromOption} />
         <b className="converter-equal"> = </b>
         <Input onChange={(e) => {
           this.onChangeTo(Number(e.target.value));
         }} size="massive" value={this.state.to} />
-        <Select options={toOptions} value={this.state.toOption} />
+        <Select onChange={(e, e2) => {
+          console.log(e2);
+          this.onChangeToOption(e2.value);
+        }} options={toOptions} value={this.state.toOption} />
       </span>
     );
   }
